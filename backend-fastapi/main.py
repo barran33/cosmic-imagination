78e070a3c070b66237a14c10bae07bfed0e1956c
barran33 <barrantesj095@gmail.com>
2026-05-21 01:22:52 -0600

    actualizar CORS

--- backend-fastapi/main.py
+++ backend-fastapi/main.py
@@ -103,7 +103,7 @@ origins = [
 ]
 
 app.add_middleware(
-    CORSMiddleware, allow_origins=origins, allow_credentials=True, 
+    CORSMiddleware, allow_origins=["*"], allow_credentials=True, 
     allow_methods=["*"], allow_headers=["*"],
 )
 
